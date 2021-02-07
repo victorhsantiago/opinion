9c0f8ea197040f0c12ca656d5920110bcbfc9602
Victor Santiago <victorhsantiago3@gmail.com>
2021-02-07 16:47:53 -0300

    Feat: add register modal component

--- src/components/ModalCreateAccount/index.vue
+++ src/components/ModalCreateAccount/index.vue
@@ -1,13 +1,157 @@
 <template>
-  <h1>Modal de Criação</h1>
+<div class="flex justify-between">
+  <h1 class="text-4xl font-black text-gray-800">Crie uma conta</h1>
+  <button
+    class="text-4xl text-gray-600 focus:outline-none"
+    @click="close"
+  >
+    &times;
+  </button>
+</div>
+
+<div class="mt-16">
+  <form class="flex flex-col gap-8" @submit.prevent="handleSubmit">
+    <label for="name" class="text-lg font-medium text-gray-800">Nome
+      <input
+        id="name"
+        name="name"
+        type="text"
+        class="block w-full px-4 py-3 text-lg bg-gray-100 border-2 border-transparent rounded"
+        :class="{ 'border-brand-danger': !!state.name.errorMessage }"
+        placeholder="Seu Nome"
+        v-model="state.name.value"
+      />
+      <span v-if="!!state.name.errorMessage" class="block font-medium text-brand-danger">
+        {{ state.name.errorMessage }}
+      </span>
+    </label>
+
+    <label for="email" class="text-lg font-medium text-gray-800">E-mail
+      <input
+        id="email"
+        name="email"
+        type="email"
+        class="block w-full px-4 py-3 text-lg bg-gray-100 border-2 border-transparent rounded"
+        :class="{ 'border-brand-danger': !!state.email.errorMessage }"
+        placeholder="seu@email.com"
+        v-model="state.email.value"
+      />
+      <span v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+        {{ state.email.errorMessage }}
+      </span>
+    </label>
+
+    <label for="password" class="text-lg font-medium text-gray-800">Senha
+      <input
+        id="password"
+        name="password"
+        type="password"
+        class="block w-full px-4 py-3 text-lg bg-gray-100 border-2 border-transparent rounded"
+        :class="{ 'border-brand-danger': !!state.password.errorMessage }"
+        placeholder="********"
+        v-model="state.password.value"
+      />
+      <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+        {{ state.password.errorMessage }}
+      </span>
+    </label>
+
+    <button
+      type="submit"
+      class="px-8
+        py-3
+        flex
+        justify-center
+        text-2xl
+        font-bold
+        text-white
+        rounded-full
+        bg-brand-main
+        focus:outline-none"
+      :class="{ 'opacity-50': state.isLoading}"
+      :disabled="state.isLoading"
+    >
+      <base-icon v-if="state.isLoading" name="loading" class="animate-spin" />
+      <span v-else>Criar conta</span>
+    </button>
+  </form>
+</div>
 </template>
 
 <script>
-export default {
+import { reactive } from 'vue'
+import { useField } from 'vee-validate'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
+import useModal from '../../hooks/useModal'
+import BaseIcon from '../BaseIcon/index.vue'
+import { validateEmptyAndEmail, validateEmptyAndLength3 } from '../../utils/validators'
+import services from '../../services'
 
+export default {
+  components: { BaseIcon },
+  setup() {
+    const router = useRouter()
+    const modal = useModal()
+    const toast = useToast()
+
+    const errorToastMessage = {
+      400: 'Ocorreu um erro ao criar a conta',
+      500: 'Ops... Aconteceu um erro inesperado',
+    }
+
+    const { value: nameValue, errorMessage: nameErrorMessage } = useField('email', validateEmptyAndLength3)
+    const { value: emailValue, errorMessage: emailErrorMessage } = useField('email', validateEmptyAndEmail)
+    const { value: passwordValue, errorMessage: passwordErrorMessage } = useField('password', validateEmptyAndLength3)
+
+    const state = reactive({
+      isLoading: false,
+      hasError: false,
+      name: {
+        value: nameValue,
+        errorMessage: nameErrorMessage,
+      },
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage,
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage,
+      },
+    })
+
+    async function handleSubmit() {
+      try {
+        state.isLoading = true
+        await services.auth.register({
+          name: state.name.value,
+          email: state.email.value,
+          password: state.password.value,
+        })
+
+        const { data } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value,
+        })
+
+        window.localStorage.setItem('token', data.token)
+        router.push({ name: 'Feedbacks' })
+        state.isLoading = false
+        modal.close()
+      } catch (error) {
+        state.isLoading = false
+        state.hasError = !!error
+
+        toast.error(errorToastMessage[error.response.status])
+      }
+    }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit,
+    }
+  },
 }
 </script>
-
-<style>
-
-</style>
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,20 @@
 export default (httpCliente) => ({
+  register: async ({ name, email, password }) => {
+    const response = await httpCliente.post('/auth/register', { name, email, password })
+    let errors = null
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText,
+      }
+    }
+
+    return {
+      data: response.data,
+      errors,
+    }
+  },
   login: async ({ email, password }) => {
     const response = await httpCliente.post('/auth/login', { email, password })
     let errors = null
